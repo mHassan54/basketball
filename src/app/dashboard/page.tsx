87a0b8451a10e0fd348411a41728e1a332ebdb3a
Muhammad Hassan <mHassan.49354@gmail.com>
2025-03-01 10:32:17 +0500

    homepage banner and matches

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import Banner from "./_components/Banner";
+import { banner } from "./_utils/banners";
+import Matches from "./_components/Matches";
+import { sampleMatches } from "./_utils/matches";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
-}
+  return (
+    <div className="flex px-8 py-5">
+      {/* left content */}
+      <div className="flex-[3] flex flex-col gap-8">
+        <Banner {...banner} />
+        <Matches matches={sampleMatches} />
+      </div>
+      {/* right bar */}
+      <div className="flex-1"></div>
+    </div>
+  );
+};
 
 export default Dashboard;
